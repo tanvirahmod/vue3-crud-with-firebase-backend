0dce1cb1e2035f8395585b304adaf1561113dbdc
tanvirahmod <tanvir.ahmod@gmail.com>
2022-09-14 09:18:38 +0600

    added firebase config

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_qk-Y58tUaz6wDXb2adu-YWNH6-MQhvI",
+  authDomain: "vue3-with-firebase-782e6.firebaseapp.com",
+  projectId: "vue3-with-firebase-782e6",
+  storageBucket: "vue3-with-firebase-782e6.appspot.com",
+  messagingSenderId: "101394048925",
+  appId: "1:101394048925:web:977c3608b36945942bb174"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
